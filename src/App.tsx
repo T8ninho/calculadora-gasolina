17fc4630fbd4f4f09473853f2ed62309f9c5f66c
T8ninho <tonisantos.contato@gmail.com>
2023-07-15 15:16:33 -0300

    modified:   src/App.css 	modified:   src/App.tsx 	modified:   src/index.css 	new file:   src/pages/GasolinaouEtanol/GasolinaouEtanol.css 	new file:   src/pages/GasolinaouEtanol/GasolinaouEtanol.tsx 	renamed:    src/assets/Logo.jpg -> src/pages/GasolinaouEtanol/Logo.jpg 	new file:   src/pages/ListadeTarefas/ListadeTarefas.css 	new file:   src/pages/ListadeTarefas/ListadeTarefas.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,96 +1,11 @@
-import LogoImg from './assets/Logo.jpg'
-import './App.css'
-import {useState, FormEvent} from 'react'
-
-interface InfoProps{
-  title: string;
-  gasolina: string | number;
-  alcool: string | number;
-}
+import "./App.css"
+import GasolinaouEtanol from "./pages/GasolinaouEtanol/GasolinaouEtanol";
+import ListadeTarefas from "./pages/ListadeTarefas/ListadeTarefas";
 
 export default function App() {
-
-  const [gasolinaInput, setGasolinaInput] = useState(1)
-  const [alcoolInput, setAlcoolInput] = useState(1)
-  const [info, setInfo] = useState<InfoProps>()
-
-  
-
-  function Calcular(event: FormEvent){
-    event.preventDefault()
-
-    let calculo = (alcoolInput / gasolinaInput)
-
-    if (calculo <= 0.7){
-      setInfo({
-        title: "Compensa usar álcool",
-        gasolina: formatarMoeda(gasolinaInput),
-        alcool: formatarMoeda(alcoolInput),
-      })
-    } else {
-      setInfo({
-        title: "Compensa usar Gasolina",
-        gasolina: formatarMoeda(gasolinaInput),
-        alcool: formatarMoeda(alcoolInput),
-      })
-    }
-  }
-
-  function formatarMoeda( valor: number) {
-    let valorFormatado = valor.toLocaleString("pt-br", 
-    {
-      style: 'currency',
-      currency: 'BRL'
-    })
-    return valorFormatado;
-  }
-
-  return (
-    <div>
-      <main className='container'>
-        <img 
-          className='logo' 
-          src={LogoImg} 
-          alt='Logo da gasolina o alcool' 
-        />
-        <h1 className='title'>Qual melhor opção?</h1>
-        <form className='form' onSubmit={Calcular}>
-          <label>Alcool (preço por litro):</label>
-          <input 
-            className='input'
-            type="number"
-            placeholder='4,9' 
-            min="1"
-            step="0.01"
-            required
-            value={alcoolInput}
-            onChange={(e) => setAlcoolInput(Number(e.target.value))}
-          />
-          <label>Gasolina (preço por litro):</label>
-          <input 
-            className='input'
-            type="number"
-            placeholder='4,9' 
-            min="1"
-            step="0.01"
-            required
-            value={gasolinaInput}
-            onChange={(e) => setGasolinaInput(Number(e.target.value))}
-          />
-          <input className='button' type='submit' value="Calcular"/>
-        </form>
-        {info && Object.keys(info).length > 0 && (
-          <section className='result'>
-          <h2 className='result-title'>{info.title}</h2>
-
-          <span>Àlcool: {info.alcool}</span>
-          <span>Gasolina: {info.gasolina}</span>
-        </section>
-        )}
-      </main>
-    </div>
+  return(
+    <>
+      <GasolinaouEtanol />
+    </>
   )
 }
-
-
-
---
+++ src/pages/ListadeTarefas/ListadeTarefas.tsx
@@ -0,0 +1,9 @@
+import "./ListadeTarefas.css"
+
+export default function ListadeTarefas() {
+	return(
+	  <div>
+		<h1>Lista de Tarefas</h1>
+	  </div>
+	)
+  }
